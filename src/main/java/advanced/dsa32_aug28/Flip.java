505797f94fc8ee3d1c2c98a71325abdd13aec240
Mithun <mithuncm@gmail.com>
2023-09-02 23:18:09 +0530

    Flip solution correction

--- src/main/java/advanced/dsa32_aug28/Flip.java
+++ src/main/java/advanced/dsa32_aug28/Flip.java
@@ -5,42 +5,37 @@ import java.util.Arrays;
 public class Flip {
     public static void main(String[] args) {
         Flip flip = new Flip();
-        int[] indexArray = flip.flip("001010");
+        int[] indexArray = flip.flip("0111000100010"); //Expected: 5, 11
+//        int[] indexArray = flip.flip("1101010001"); //Expected: 3, 9
         System.out.println(Arrays.toString(indexArray));
     }
 
     public int[] flip(String A) {
-        boolean areAllOnes = true;
-        for (char c : A.toCharArray()) {
-            if (c == '0') {
-                areAllOnes = false;
-                break;
-            }
-        }
-        if (areAllOnes) return new int[0];
+        if (!A.contains("0")) return new int[0];
 
+        int l = 0;
+        int r = 0;
         int left = 0;
-        int right = 0;
-        int zero = 0;
+        int zeroCount = 0;
         int maxZeros = 0;
         for (int i = 0; i < A.length(); i++) {
             char c = A.charAt(i);
             if (c == '0') {
-                zero++;
-                if (zero > maxZeros) {
-                    maxZeros = zero;
-                    right = i;
+                zeroCount++;
+                if (zeroCount > maxZeros) {
+                    maxZeros = zeroCount;
+                    l = left;
+                    r = i;
                 }
             } else {
-                zero--;
+                zeroCount--;
             }
-            if (zero < 0) {
-                zero = 0;
-                left = i;
-                right = i;
+            if (zeroCount < 0) {
+                zeroCount = 0;
+                left = i + 1;
             }
         }
         //L and R start from 1, as per the question. Correcting the indexing
-        return new int[]{left + 1, right + 1};
+        return new int[]{l + 1, r + 1};
     }
 }
