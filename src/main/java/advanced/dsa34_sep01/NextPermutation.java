6b87a2ea3dcc7323ba06e534d6dfdfcca48244df
Mithun <mithuncm@gmail.com>
2023-09-05 22:19:24 +0530

    NextPermutation isn't right yet

---
+++ src/main/java/advanced/dsa34_sep01/NextPermutation.java
@@ -0,0 +1,41 @@
+package advanced.dsa34_sep01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Implement the next permutation, which rearranges numbers into the numerically next greater permutation of numbers for a given array A of size N.
+ * <p>
+ * If such arrangement is not possible, it must be rearranged as the lowest possible order, i.e., sorted in ascending order.
+ */
+public class NextPermutation {
+
+    public static void main(String[] args) {
+        NextPermutation nextPermutation = new NextPermutation();
+        ArrayList<Integer> result = nextPermutation.nextPermutation(new ArrayList<>(Arrays.asList(
+                7, 3, 4, 5, 2, 1, 3, 3, 1
+        )));
+        System.out.println("result = " + result);
+
+    }
+
+    public ArrayList<Integer> nextPermutation(ArrayList<Integer> A) {
+        //734521313 -> Start from the rightmost element. The first element less than it should be swapped
+        for (int i = A.size() - 1; i > 0; i--) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (A.get(j) < A.get(i)) {
+                    swap(A, i, j);
+                    return A;
+                }
+            }
+        }
+
+        return A;
+    }
+
+    void swap(ArrayList<Integer> list, int index1, int index2) {
+        int temp = list.get(index1);
+        list.set(index1, list.get(index2));
+        list.set(index2, temp);
+    }
+}
