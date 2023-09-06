b36a75d48efab6de542768da421f6844dc55ae3d
Mithun <mithuncm@gmail.com>
2023-09-06 23:55:57 +0530

    NextPermutation solution needs to be corrected. MaximumBitwiseAndPair solution added - part of class's discussion

---
+++ src/main/java/advanced/dsa36_sep06/MaximumBitwiseAndPair.java
@@ -0,0 +1,43 @@
+package advanced.dsa36_sep06;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MaximumBitwiseAndPair {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(26, 13, 23, 28, 27, 7, 25);
+        for (Integer num : numbers) {
+            System.out.println(Integer.toBinaryString(num));
+        }
+        int maximum = solve(numbers);
+        System.out.println("maximum = " + maximum);
+    }
+
+    private static int solve(List<Integer> numbers) {
+        int ans = 0;
+        for (int i = 31; i >= 0; i--) {
+            int count = countElementsIthBitSet(numbers, i);
+            if (count >= 2) {
+                int ithBit = (1 << i);
+                ans = ans + (1 << i);
+                for (int j = 0; j < numbers.size(); j++) {
+                    //If count >= 2, set numbers whose ith bit is zero to 0.
+                    if ((numbers.get(j) & ithBit) == 0) {
+                        numbers.set(j, 0);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    private static int countElementsIthBitSet(List<Integer> numbers, int i) {
+        int count = 0;
+        for (Integer num : numbers) {
+            if ((num & 1 << i) != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
