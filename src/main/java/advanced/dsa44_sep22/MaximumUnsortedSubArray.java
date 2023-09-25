b6d7821e781b72438f9e361f7d3cf02f7c413781
Mithun <mithuncm@gmail.com>
2023-09-25 20:28:03 +0530

    MaxAndMinMagicNumber: Need to factor in the case where a smaller number follows another smaller number

--- src/main/java/advanced/dsa44_sep22/MaximumUnsortedSubArray.java
+++ src/main/java/advanced/dsa44_sep22/MaximumUnsortedSubArray.java
@@ -6,8 +6,9 @@ import java.util.Collections;
 
 public class MaximumUnsortedSubArray {
     public static void main(String[] args) {
-//        ArrayList<Integer> indices = subUnsort(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5)));
-        ArrayList<Integer> indices = subUnsort(new ArrayList<>(Arrays.asList(1, 3, 2, 4, 5)));
+//        ArrayList<Integer> indices = subUnsort(new ArrayList<>(Arrays.asList(3, 3, 4, 5, 5, 9, 11, 13, 14, 15, 15, 16, 15, 20, 16))); //11, 14 expected
+        ArrayList<Integer> indices = subUnsort(new ArrayList<>(Arrays.asList(1,2,2,3,3,5,6,6,14,17,18,17,18,15,15,17,19,14,19,18))); // 9, 19 expected
+//        ArrayList<Integer> indices = subUnsort(new ArrayList<>(Arrays.asList(1, 3, 2, 4, 5)));
         System.out.println("indices = " + indices);
     }
 
@@ -24,7 +25,8 @@ public class MaximumUnsortedSubArray {
             if (current >= previous) {
                 i++;
             } else {
-                while (current < A.get(i - 1)) {
+                i--;
+                while (i != -1 && current < A.get(i)) {
                     i--;
                 }
                 break;
@@ -40,12 +42,14 @@ public class MaximumUnsortedSubArray {
             if (current <= previous) {
                 j--;
             } else {
-                while (current > A.get(j + 1)) {
+                j++;
+                while (j != A.size() && current > A.get(j)) {
                     j++;
                 }
                 break;
             }
         }
-        return new ArrayList<>(Arrays.asList(i, j));
+        //Current is greater than A[j], but increment happens to the next
+        return new ArrayList<>(Arrays.asList(i + 1, j - 1));
     }
 }
