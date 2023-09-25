3e4246a955c96db934d695e2762b873586f95891
Mithun <mithuncm@gmail.com>
2023-09-25 19:46:41 +0530

    CountSort solution added. MaximumUnsortedSubArray needs to be modified

---
+++ src/main/java/advanced/dsa44_sep22/MaximumUnsortedSubArray.java
@@ -0,0 +1,55 @@
+package advanced.dsa44_sep22;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class MaximumUnsortedSubArray {
+    public static void main(String[] args) {
+        ArrayList<Integer> indices = subUnsort(new ArrayList<>(Arrays.asList(1, 3, 2, 4, 5)));
+        System.out.println("indices = " + indices);
+    }
+
+    public static ArrayList<Integer> subUnsort(ArrayList<Integer> A) {
+        if (A.size() == 1) {
+            return new ArrayList<>(Collections.singletonList(-1));
+        }
+        //From right maintain the index of the smallest element, from left maintain the index of the largest element
+        ArrayList<Integer> largestFromLeft = new ArrayList<>();
+        largestFromLeft.add(A.get(0));
+        int i = 1;
+        while (i < A.size()) {
+            Integer current = A.get(i);
+            Integer previous = A.get(i - 1);
+            if (current > previous) {
+                largestFromLeft.add(i, current);
+                i++;
+            } else {
+                while (current < largestFromLeft.get(i - 1)) {
+                    i--;
+                }
+                break;
+            }
+        }
+        if (i == A.size()) {
+            return new ArrayList<>(Collections.singletonList(-1));
+        }
+        ArrayList<Integer> smallestFromRight = new ArrayList<>();
+        smallestFromRight.add(A.get(A.size() - 1));
+        int j = A.size() - 2;
+        while (j >= 0) {
+            Integer current = A.get(j);
+            Integer previous = A.get(j + 1);
+            if (current < previous) {
+                smallestFromRight.add(j, current);
+                j--;
+            } else {
+                while (current > smallestFromRight.get(j + 1)) {
+                    j++;
+                }
+                break;
+            }
+        }
+        return new ArrayList<>(Arrays.asList(i, j));
+    }
+}
