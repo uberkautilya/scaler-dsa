c22978babcdaf233fc5246d3dbc30d184f288c6c
Mithun <mithuncm@gmail.com>
2023-09-29 22:59:49 +0530

    num << 1 can create TLE or wrong answer when done beyond int capacity

--- src/main/java/advanced/contest_sep29/MagicalNumber5.java
+++ src/main/java/advanced/contest_sep29/MagicalNumber5.java
@@ -14,22 +14,12 @@ public class MagicalNumber5 {
 
     private ArrayList<Integer> solve(ArrayList<Integer> A) {
         ArrayList<Integer> result = new ArrayList<>();
-        int size = A.size();
-        for (int i = 0; i < size; i++) {
-            boolean divisibleBy5 = check(A, i);
-            result.add(divisibleBy5 ? 1 : 0);
+        int num = A.get(0);
+
+        for (int current: A) {
+            num = (num << 1) + current;
+            result.add(num % 5 == 0 ? 1 : 0);
         }
         return result;
-    }
-
-    private boolean check(ArrayList<Integer> A, int end) {
-        int num = 0;
-        int posVal = 1;
-        while (end >= 0) {
-            num += A.get(end) * posVal;
-            end--;
-            posVal *= 2;
-        }
-        return num % 5 == 0;
     }
 }
