4299d3c97a7bfb21f99dbb20abe61f57efedaad8
Mithun <mithuncm@gmail.com>
2023-09-29 23:29:28 +0530

    The right solution as per the correct understanding for MagicalNumber5 added

--- src/main/java/advanced/contest_sep29/MagicalNumber5.java
+++ src/main/java/advanced/contest_sep29/MagicalNumber5.java
@@ -3,23 +3,29 @@ package advanced.contest_sep29;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * For all decimal numbers formed, including 0 that is divisible by 5, the result needs to have 1
+ */
 public class MagicalNumber5 {
     public static void main(String[] args) {
 //        ArrayList<Integer> result = new MagicalNumber5().solve(new ArrayList<>(Arrays.asList(0, 0, 1, 1, 1, 1)));
 //        ArrayList<Integer> result = new MagicalNumber5().solve(new ArrayList<>(Arrays.asList(0, 0, 1, 0, 1, 1)));
 //        ArrayList<Integer> result = new MagicalNumber5().solve(new ArrayList<>(Arrays.asList(1, 0, 1)));
-        ArrayList<Integer> result = new MagicalNumber5().solve(new ArrayList<>(Arrays.asList(1, 0, 1, 0, 0)));
+        ArrayList<Integer> result = new MagicalNumber5().solve(new ArrayList<>(Arrays.asList(0, 0, 0, 1)));
+//        ArrayList<Integer> result = new MagicalNumber5().solve(new ArrayList<>(Arrays.asList(1, 0, 1, 0, 0)));
         System.out.println("result = " + result);
     }
 
     private ArrayList<Integer> solve(ArrayList<Integer> A) {
         ArrayList<Integer> result = new ArrayList<>();
-        int num = A.get(0);
+        int num = 0;
 
-        for (int current: A) {
+        for (int current : A) {
             //The below step cannot be done - Left Shift can create wrong values
-            num = (num << 1) + current;
-            result.add(num % 5 == 0 ? 1 : 0);
+            //num = (num << 1) + current;
+
+            num = ((num << 1) + current) % 5;
+            result.add(num == 0 ? 1 : 0);
         }
         return result;
     }
