022a8271e5bf58c7999ee01d3f71c700b652126c
Mithun <mithuncm@gmail.com>
2023-10-24 13:00:03 +0530

    PairsWithGivenDifference solution added

--- src/main/java/advanced/dsa55_oct11/PairsWithGivenDifference.java
+++ src/main/java/advanced/dsa55_oct11/PairsWithGivenDifference.java
@@ -10,32 +10,39 @@ public class PairsWithGivenDifference {
                 8, 5, 1, 10, 5, 9, 9, 3, 5, 6, 6, 2, 8, 2, 2, 6, 3, 8, 7, 2, 5, 3, 4, 3, 3, 2, 7, 9, 6, 8, 7, 2, 9, 10, 3, 8, 10, 6, 5, 4, 2, 3
         ));
         numList = new ArrayList<>(Arrays.asList(5, 2, 2, 4, 9, 8, 5, 3, 8, 8, 10, 4, 2, 10, 9, 7, 6, 1, 3, 9, 7, 1, 3, 5));
+        numList = new ArrayList<>(Arrays.asList(1, 8, 2, 8, 8, 8, 8, 4, 4, 6, 10, 10, 9, 2, 9, 3, 7));
+        int b = 3;
+        b = 1;
+        b = 0;
         Integer result = pairsWithGivenDifference.solve(
-                numList, 3);
+                numList, b);
         System.out.println("result = " + result);
 
     }
 
     public int solve(ArrayList<Integer> A, int B) {
+        int size = A.size();
         int count = 0;
         A.sort((a, b) -> a - b);
-        int p1 = 0;
-        int p2 = 1;
-        while (p2 < A.size() && p1 < p2) {
-            int n1 = A.get(p1);
-            int n2 = A.get(p2);
-            if (n2 - n1 == B) {
+        int p1 = 0, p2 = 1;
+
+        while (p2 < size && p1 <= p2) {
+            int num1 = A.get(p1);
+            int num2 = A.get(p2);
+            if (num2 - num1 == B && p1 != p2) {
                 count++;
-                while (p2 < A.size() && n2 == A.get(p2)) {
+                while ((p2 + 1) < size &&
+                        num2 == A.get(p2 + 1)) {
                     p2++;
                 }
-                while (p1 < p2 && n1 == A.get(p1)) {
+                while ((p1 + 1) < p2 &&
+                        num1 == A.get(p1 + 1)) {
                     p1++;
                 }
             }
-            if (n2 - n1 < B) {
+            if (num2 - num1 <= B && (p2 + 1) < size) {
                 p2++;
-            } else {
+            }else {
                 p1++;
             }
         }
