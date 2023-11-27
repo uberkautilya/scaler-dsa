1cf3356181a4644e48bd93159e578b4bd1bafa8c
Mithun <mithuncm@gmail.com>
2023-11-27 23:44:10 +0530

    Skeleton of CustomLL and CustomQueue added.

---
+++ src/main/java/advanced/module6/linked_lists/CustomLL.java
@@ -0,0 +1,9 @@
+package advanced.module6.linked_lists;
+
+import advanced.module6.Node;
+
+public class CustomLL {
+    Node head;
+    Node tail;
+
+}
---
+++ src/main/java/advanced/module6/queues/CustomQueue.java
@@ -0,0 +1,7 @@
+package advanced.module6.queues;
+
+public class CustomQueue {
+    public void enque(Integer x) {
+
+    }
+}
