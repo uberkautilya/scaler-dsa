ca082664445264ef34bf8987db7d0c19914c2723
Mithun <mithuncm@gmail.com>
2024-01-27 20:16:32 +0530

    Graphs Traversal: BreadthFirstSearch Traversal solution corrected.

--- src/main/java/advanced/module7/graphs/BreadthFirstSearchTraversal.java
+++ src/main/java/advanced/module7/graphs/BreadthFirstSearchTraversal.java
@@ -10,35 +10,36 @@ public class BreadthFirstSearchTraversal {
         ArrayList<ArrayList<Integer>> adjoinList = new ArrayList<>();
         ArrayList<Integer> intList;
 
+        intList = new ArrayList<>(Arrays.asList(6, 3));
+        adjoinList.add(new ArrayList<>(intList));
+        intList = new ArrayList<>(Arrays.asList(4, 0));
+        adjoinList.add(new ArrayList<>(intList));
+        intList = new ArrayList<>(Arrays.asList(6, 5));
+        adjoinList.add(new ArrayList<>(intList));
+        intList = new ArrayList<>(Arrays.asList(2, 6));
+        adjoinList.add(new ArrayList<>(intList));
+        intList = new ArrayList<>(Arrays.asList(4, 1));
+        adjoinList.add(new ArrayList<>(intList));
+        intList = new ArrayList<>(Arrays.asList(4, 5));
+        adjoinList.add(new ArrayList<>(intList));
         intList = new ArrayList<>(Arrays.asList(1, 3));
         adjoinList.add(new ArrayList<>(intList));
-        intList = new ArrayList<>(Arrays.asList(2, 0));
-        adjoinList.add(new ArrayList<>(intList));
-        intList = new ArrayList<>(Arrays.asList(3, 5));
-        adjoinList.add(new ArrayList<>(intList));
-        intList = new ArrayList<>(Arrays.asList(4, 6));
-        adjoinList.add(new ArrayList<>(intList));
-        intList = new ArrayList<>(Arrays.asList(4, 6));
-        adjoinList.add(new ArrayList<>(intList));
-        intList = new ArrayList<>(Arrays.asList(4, 6));
-        adjoinList.add(new ArrayList<>(intList));
-        intList = new ArrayList<>(Arrays.asList(4, 6));
-        adjoinList.add(new ArrayList<>(intList));
+        boolean[] visited = new boolean[7];
 
         Node[] nodeArray = {
+                new Node(100, "Node100"),
                 new Node(101, "Node101"),
                 new Node(102, "Node102"),
                 new Node(103, "Node103"),
                 new Node(104, "Node104"),
                 new Node(105, "Node105"),
-                new Node(106, "Node106"),
-                new Node(100, "Node100")
+                new Node(106, "Node106")
         };
-        for (int i = 0; i < nodeArray.length; i++) {
+        for (int index = 0; index < nodeArray.length; index++) {
             breadthFirstSearch(
                     nodeArray,
-                    i,
-                    new boolean[7],
+                    index,
+                    visited,
                     new CustomQueue(),
                     adjoinList
             );
@@ -46,7 +47,7 @@ public class BreadthFirstSearchTraversal {
     }
 
     public static void breadthFirstSearch(Node[] nodeArray, Integer index, boolean[] visited,
-                                          Queue<Node> queue, ArrayList<ArrayList<Integer>> adjoinNodesArray) {
+                                          Queue<Node> queue, ArrayList<ArrayList<Integer>> adjoinNodesList) {
         if (visited[index]) {
             return;
         }
@@ -57,8 +58,8 @@ public class BreadthFirstSearchTraversal {
             Node n = queue.remove();
             System.out.println(n);
 
-            //adjoinNodesArray.get(index) gives the list of nodes to which the current index node is connected
-            for (Integer adjoinNodeIndex : adjoinNodesArray.get(index)) {
+            //adjoinNodesList.get(index) gives the list of nodes to which the current index node is connected
+            for (Integer adjoinNodeIndex : adjoinNodesList.get(index)) {
                 if (!visited[adjoinNodeIndex]) {
                     visited[adjoinNodeIndex] = true;
                     queue.add(nodeArray[adjoinNodeIndex]);
