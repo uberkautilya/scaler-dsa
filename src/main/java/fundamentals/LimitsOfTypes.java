60c0ff134aaf7a0afb2cc9b5c8a0f2f923268ced
Mithun <mithuncm@gmail.com>
2024-02-23 23:42:32 +0530

    Fundamentals: Added limits of Integer and Long

---
+++ src/main/java/fundamentals/LimitsOfTypes.java
@@ -0,0 +1,10 @@
+package fundamentals;
+
+public class LimitsOfTypes {
+    public static void main(String[] args) {
+        System.out.println("Long.MAX_VALUE = " + Long.MAX_VALUE);
+        // 9_223_372_036_854_775_807
+        System.out.println("Integer.MAX_VALUE = " + Integer.MAX_VALUE);
+        //2_147_483_647
+    }
+}
