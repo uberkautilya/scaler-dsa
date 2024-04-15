a73ac4014e4137cf91243b404fab9d7bafb87fb3
Mithun <mithuncm@gmail.com>
2024-04-15 23:24:09 +0530

    SQL 10: Transactions class queries added

---
+++ src/db/SQL_10_Transactions.sql
@@ -0,0 +1,28 @@
+-- See active transactions in MySQL
+SELECT * FROM information_schema.innodb_trx
+
+-- Session 1
+show variables like 'autocommit';
+show variables like 'transaction_isolation';
+
+set autocommit = 0;
+
+-- start transactions;
+start transaction;
+use sakila;
+select * from sakila.film where film_id = 20;
+update film set title = 'Harry Potter' where film_id = 20;
+rollback;
+
+
+-- Session 2
+show variables like 'autocommit';
+show variables like 'transaction_isolation';
+
+set autocommit = 0;
+set session transaction isolation level read uncommitted;
+
+-- start transaction
+start transaction;
+use sakila;
+select * from film where film_id = 20;
