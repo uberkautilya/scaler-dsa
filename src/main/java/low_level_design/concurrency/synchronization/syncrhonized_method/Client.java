52687c311434cb4ddd0e457d5fcce847638a3781
Mithun <mithuncm@gmail.com>
2024-05-22 00:31:36 +0530

    LLD Synchronization: Optimized imports

--- src/main/java/low_level_design/concurrency/synchronization/syncrhonized_method/Client.java
+++ src/main/java/low_level_design/concurrency/synchronization/syncrhonized_method/Client.java
@@ -4,8 +4,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class Client {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
