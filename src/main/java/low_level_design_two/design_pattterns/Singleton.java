984514febb791a8d6241018a4cc679908b4f724c
Mithun <mithuncm@gmail.com>
2024-06-10 23:19:22 +0530

    LLD 2: Singleton Design Pattern solution added

---
+++ src/main/java/low_level_design_two/design_pattterns/Singleton.java
@@ -0,0 +1,18 @@
+package low_level_design_two.design_pattterns;
+
+public class Singleton {
+    //static field initialization is done by the main thread at the time of class loading
+    private static final Singleton instance = new Singleton();
+    String url;
+    String name;
+
+    private Singleton() {
+        //Instance cannot be configured at runtime, if creation of object requires values which are available only at runtime
+        this.url = "Value 101";
+        this.name = "Name 101";
+    }
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
