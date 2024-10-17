710324941d4b818c3d826239f7b6eb77baefc0a6
Mithun <mithun.mohandas@aspiresys.com>
2024-10-17 17:59:06 +0530

    Memoization solution added to PerfectSquareSum.java

--- src/main/java/advanced/module7/dynamicprogramming/PerfectSquareSum.java
+++ src/main/java/advanced/module7/dynamicprogramming/PerfectSquareSum.java
@@ -14,19 +14,35 @@ public class PerfectSquareSum {
         Integer[] dp = new Integer[number + 1];
         //Max no. of perfect squares whose sum is number is 1^2 + 1^2 +.. = number. Max count is number.
         Arrays.fill(dp, Integer.MAX_VALUE);
-        int count = findCount(number, dp);
-        System.out.println("count = " + count);
+        int count = findCountUsingDP(number, dp);
+
+        System.out.println("Count with top-down dp: " + count);
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        System.out.println("Count with memoization: " + findCountUsingMemoization(number, dp));
     }
 
-    public static int findCount(int number, Integer[] dp) {
+    public static int findCountUsingDP(int number, Integer[] dp) {
         if (number == 0) {
             return 0;
         }
         if (dp[number] == Integer.MAX_VALUE) {
             for (int i = 1; (i * i) <= number; i++) {
                 dp[number] = Math.min(
-                        dp[number], 1 + findCount(number - (i * i), dp)
+                        dp[number], 1 + findCountUsingDP(number - (i * i), dp)
                 );
+            }
+        }
+        return dp[number];
+    }
+
+    public static int findCountUsingMemoization(int number, Integer[] dp) {
+        if (number == 0) {
+            return 0;
+        }
+        dp[0] = 0;
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
             }
         }
         return dp[number];
